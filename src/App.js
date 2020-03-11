2cd60c9bc96599909bb6c9c5dd470ffc3a0a1656
SuhYPark <suhpark@gmail.com>
2020-03-10 20:52:24 -0400

    second commit - reset working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class Ticker extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      count: 0,
+      status: "Pause",
+    }
+  }
+
+  clear = () => {
+    this.setState ({
+      count: 0,
+    })
+  }
+
+  pause = () => {
+    if(this.state.status == "Pause"){
+      this.setState({
+        status: "Run",
+
+
+      });
+    }
+    else {
+      this.setState({
+        status: "Pause"
+      });
+    }
+
+    clearInterval (() => {
+      
+    })
+
+  }
+//lifeCycle methods
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      })
+    }, 1000);
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+  //this is where we check a condition if it's true or false
+  //and onluy allow a rerender if the condition is true
+    return (nextState.count % 3 === 0);
+
+    // if(nextState.count % 3 == 0){
+    //   return true;
+    //   console.log("why", nextState.count)
+    // }
+    // else{
+    //   return false;
+    // }
+    //console.log("this is the nextState value: ", nextState.count)
+
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="container">
+          <div className="text">The ticker is: {this.state.count}</div>
+          <button type="button" onClick={this.clear}>Reset the Ticker</button>&nbsp;&nbsp;
+          <button type="button" onClick={this.pause}>{this.state.status} the Ticker</button>
+        </div>
+      </div>
+    );
+  };
+};
+
+export default Ticker;
